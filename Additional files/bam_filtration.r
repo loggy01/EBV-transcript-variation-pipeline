3a504b0a4c17a390b6a35452f73f6094cd68ffe7
loggy01 <116520578+loggy01@users.noreply.github.com>
2023-04-02 23:11:45 +0100

    update of comment
    see above

--- Additional files/bam_filtration.r
+++ Additional files/bam_filtration.r
@@ -13,7 +13,7 @@ args <- commandArgs(trailingOnly = TRUE)
 bam_files <- strsplit(args[1], ",")[[1]] # user bam files (list of directories)
 bam_names <- strsplit(args[2], ",")[[1]] # names matching the bam files (list of strings)
 data_index <- as.numeric(args[3]) # index representing the column in each bam file that will be filtered against (see https://samtools.github.io/hts-specs/SAMv1.pdf) (integer)
-data_charateristics <- strsplit(args[4], ",")[[1]] # single column (with column header) text files matching the bam files. The data here is applied across the specified data_index for the matching bam file (list of directories)
+data_charateristics <- strsplit(args[4], ",")[[1]] # single column (with column header) text files matching the bam files. The data here is applied across the specified data_index for the matching bam file. An example file has been given in the folder. (list of directories)
 comparison <- args[5] # TRUE or FALSE. If TRUE, the bam files will be filtered to include only reads that match the data in the data_charateristics file. If FALSE, the bam files will be filtered to include only reads that do not match the data in the data_charateristics file (boolean)
 
 for(i in 1:length(bam_files)){
